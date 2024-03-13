e6ad9aeb37092fb7129eb0982165797c17a87208
Karan.Pawar <karan.pawar@nw18.com>
2024-03-13 15:19:08 +0530

    slick dots taken down

--- send_email.php
+++ send_email.php
@@ -1,47 +1,9 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
-
-require 'path/to/PHPMailer/src/Exception.php';
-require 'path/to/PHPMailer/src/PHPMailer.php';
-require 'path/to/PHPMailer/src/SMTP.php';
-
-// Check if the form was submitted
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-
-    // Retrieve form data
     $name = $_POST['name'];
     $email = $_POST['email'];
-    $message = $_POST['message'];
+    $subject = $_POST['subject'];
 
-    // Create a new PHPMailer instance
-    $mail = new PHPMailer();
-
-    // SMTP configuration
-    $mail->isSMTP();
-    $mail->Host = 'smtp.gmail.com';
-    $mail->SMTPAuth = true;
-    $mail->Username = 'your_email@gmail.com'; // Your Gmail address
-    $mail->Password = 'your_password'; // Your Gmail password
-    $mail->SMTPSecure = 'tls';
-    $mail->Port = 587;
-
-    // Sender and recipient settings
-    $mail->setFrom('your_email@gmail.com', 'Your Name');
-    $mail->addAddress('recipient@example.com', 'Recipient Name');
-
-    // Email content
-    $mail->isHTML(true);
-    $mail->Subject = 'New Message from Contact Form';
-    $mail->Body = "Name: $name <br> Email: $email <br> Message: $message";
-
-    // Send email
-    if (!$mail->send()) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-    } else {
-        echo 'Message has been sent';
-    }
-}
+    mail($email, $subject, $name);
+    echo "mail sent";
 ?>
